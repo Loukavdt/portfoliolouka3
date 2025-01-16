5c6dae912acee00c2eb850ee2625506b63d06d7a
Loukavdt <loukavdt2002@gmail.com>
2025-01-16 15:16:34 +0100

    footer aanpassingen

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -4,17 +4,17 @@
         <!-- Portfolio Section -->
         <div class="footer-item text-center">
           <p>Thank you for reviewing my</p>
-          <h1 class="text-3xl mt-2">Portfolio</h1>
+          <h1 class="text-2xl mt-2">Portfolio</h1>
         </div>
   
         <!-- Contact Me Section -->
         <div class="footer-item text-center">
-          <h1 class="text-xl">Contact Me</h1>
+          <h1 class="text-xl">Contact</h1>
           <div class="flex justify-center space-x-4 mt-4">
             <!-- Email Link -->
             <NuxtLink
               to="mailto:loukavdt2002@gmail.com?subject=Hello%20Louka&body=This%20is%20a%20test%20email!"
-              class="border border-black hover:border-white hover:text-white transition-all duration-100 rounded-full px-4 py-2 text-sm"
+              class="border border-white hover:border-white hover:text-white transition-all duration-100 rounded-full px-4 py-2 text-sm"
             >
               Email
             </NuxtLink>
